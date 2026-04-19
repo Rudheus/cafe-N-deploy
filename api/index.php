edbacec6148b59acaea09a59f91f4243947531b7
Rudheus <iskandarahman1@gmail.com>
2026-04-19 19:30:23 +0700

    Fix by claude 5

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+use Illuminate\Http\Request;
+define('LARAVEL_START', microtime(true));
+// ... sisanya tetap sama
 require __DIR__ . '/../public/index.php';
